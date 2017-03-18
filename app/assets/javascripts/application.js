4251daf6becb943ea4b095940259a724be530fea
Thiago Belem <contato@thiagobelem.net>
2017-03-18 18:57:16 -0300

    Add basic layout markup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's 
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
 // vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
@@ -14,3 +14,7 @@
 //= require turbolinks
 //= require jquery
 //= require_tree .
+
+$(document).on('click', '.nav-toggle', function() {
+  $('.nav-menu').toggleClass('is-active');
+});
