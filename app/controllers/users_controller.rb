5a3fd033fe7f901657a5c7a17501f6d53867c9a1
Thiago Belem <contato@thiagobelem.net>
2017-03-19 02:50:06 -0300

    Create registration form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < Clearance::UsersController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      sign_in @user
+      redirect_to root_path, notice: "Bem-vindo(a), #{@user.name}"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
