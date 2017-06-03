1fcdea695205bf22473871d064f58e620ed502ec
Thiago Belem <contato@thiagobelem.net>
2017-06-03 12:39:52 -0300

    Hide user routes on home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
     @routes = Route.enabled.order(created_at: :desc).limit(10)
+    @routes = @routes.where.not(user: current_user) if signed_in?
   end
 end
